73a2b5de4090c7a0741d3c055bf64b8a33a875c6
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-03-09 21:44:26 +0530

    add Token class and token_types

--- lexer.py
+++ lexer.py
@@ -1,2 +1,20 @@
+token_types = {
+    "keyword":"keyword",
+    "identifier": "identifier",
+    "string_literal": "string_literal",
+    "number_literal": "number_literal",
+    "left_paren": "left_paren",
+    "right_paren": "right_paren",
+    "left_brace": "left_brace",
+    "right_brace": "right_brace",
+    "semicolon": "semicolon"
+}
+
+class Token:
+    def __init__(self,type,value,index):
+        self.type = type
+        self.value = value
+        self.index = index
+
 def lex(src):
-    print("tokens!!!")
+    pass
