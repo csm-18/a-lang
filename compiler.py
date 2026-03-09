8b857f841a909b6df6044d295076205029156df8
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-03-09 21:17:54 +0530

    read .a source file to string

--- compiler.py
+++ compiler.py
@@ -1,3 +1,10 @@
 def compile(filename: str):
-    print("compiling...")            
-    
+    code = "" 
+    #read .a source file to string
+    try:
+        with open(filename, "r") as f:
+            code = f.read()
+    except:
+        print("error: Unable to read file",filename)        
+
+    print(code)    
